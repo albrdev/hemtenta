eab55aa4b919990345c23ec67972ebf09f087279
albrdev <albrdev@gmail.com>
2018-12-07 12:20:04 +0100

    iterative versions

--- mystring/mystring.c
+++ mystring/mystring.c
@@ -60,23 +60,45 @@ void _mystr_debug_print(mystr *S) {
 
 int mystr_len(mystr *S) {
 	// Implementera!!
-    if(!S) return 0;
-    return mystr_len(S->tail) + 1;
+    int count = 0;
+    while(S) {
+        count++;
+        S = mystr_get_tail(S);
+    }
+
+    return count;
 }
 
 mystr *mystr_dup(mystr *S) {
 	// Implementera!!
+    // Way more work in iterative version of this
     if(!S) return 0;
-    return mystr_new_char(mystr_get_char(S), mystr_dup(mystr_get_tail(S)));
-}
 
-static mystr *_mystr_inverse(mystr *S, mystr *R) {
-    // Implementera!!
-    if(!S) return R;
-    return _mystr_inverse(mystr_get_tail(S), mystr_new_char(mystr_get_char(S), R));
+    mystr *prev = mystr_new_char(mystr_get_char(S), 0); // Make first outside loop to save a pointer to it
+    mystr *new = 0;
+    mystr *first = prev; // We'll need a pointer to the first
+    S = mystr_get_tail(S);
+    while(S)
+    {
+        new = mystr_new_char(mystr_get_char(S), 0); // Create the next nodes
+        mystr_set_tail(prev, new); // Set previous node's tail to the new one
+        prev = new;
+        S = mystr_get_tail(S);
+    }
+
+    return first;
 }
 
 mystr *mystr_inverse(mystr *S) {
 	// Implementera!!
-    return _mystr_inverse(S, 0);
+    if(!S) return 0;
+
+    mystr *result = 0;
+    while(S)
+    {
+        result = mystr_new_char(mystr_get_char(S), result);
+        S = mystr_get_tail(S);
+    }
+
+    return result;
 }
