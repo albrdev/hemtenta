15abb12775da6ffec90bc4743b4ace69a65cc2cb
albrdev <albrdev@gmail.com>
2018-12-07 04:33:56 +0100

    shell sort

--- shellsort/shellsort.ino
+++ shellsort/shellsort.ino
@@ -42,15 +42,40 @@ void dumpTable() {
 
 void verifyTable() {
     for(int i = 1; i < SENSOR_SIZE; i++) {
-        if(sensorTab[i].tim < sensorTab[i-1].tim) {
+        if(sensorTab[i].value < sensorTab[i-1].value) {
             Serial.print("ERROR: array unsorted at index ");
             Serial.println(i);
         }
     }
 }
 
+/*  shellSort: Sorts an array of struct sensor_S by value in ascending order
+ *  Sources: https://www.geeksforgeeks.org/shellsort/, https://rosettacode.org/wiki/Sorting_algorithms/Shell_sort
+ */
 int shellSort(struct sensor_S *arr, int size) {
     // Implementera Shell sort här!
+    // The examples I've been looking at are very similar in code, I've chosen to divide by 2, although Marcin Ciura's sequence is probably faster
+    int h, i, j;
+    struct sensor_S tmp;
+
+    if(!arr || size < 0) return -1; // Function returns int for some reason, so let's use it for something
+
+    // Sort with half the size of the previous gap, starting with half the size of the array
+    for(h = size / 2; h > 0; h /= 2)
+    {
+      for(i = h; i < size; i++)
+      {
+        tmp = arr[i]; // Move this value to its sorted location (in this gap sequence)
+        for(j = i; j >= h && arr[j - h].value > tmp.value; j -= h) // By shifting higher values in gap to the right, consequently making room for the temporary value
+        {
+          arr[j] = arr[j - h]; // Shift
+        }
+
+        arr[j] = tmp; // Temporary value's new, sorted location
+      }
+    }
+
+    return 0;
 }
 
 void loop() {
@@ -60,6 +85,7 @@ void loop() {
     Serial.print(tick);
     Serial.print(" värde = ");
     Serial.println(sensorVal);
+    //Serial.flush(); // I noticed that when i commented out the previous printing lines I got increased performance, so I put this here to make sure the buffer is flushed before going into the sorting, it sped up the time reported by about 5-6 times (~35ms down to ~6ms). I tried to put it inside the following 'if' statement, just before the sort, but that doubled the time instead for some unknown reason (maybe the flush is processed in another thread that jams sorting?).
     sensorTab[sensorIx].tim = tick;
     sensorTab[sensorIx].value = sensorVal;
     sensorIx++;
